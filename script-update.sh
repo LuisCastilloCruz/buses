a006925b8b5d12ab2c82edeeef07411d38c9a698
jefelon <ronilson_2006_2@hotmail.com>
2020-08-18 11:54:22 -0500

    important laravel zip add

--- script-update.sh
+++ script-update.sh
@@ -6,6 +6,7 @@ SERVICE_NUMBER=${2:-'1'}
 git pull origin "$BRANCH"
 
 docker-compose exec -T fpm$SERVICE_NUMBER composer install
+docker-compose exec -T fpm$SERVICE_NUMBER composer require zanysoft/laravel-zip
 docker-compose exec -T fpm$SERVICE_NUMBER php artisan migrate
 docker-compose exec -T fpm$SERVICE_NUMBER php artisan tenancy:migrate
 docker-compose exec -T fpm$SERVICE_NUMBER php artisan cache:clear
