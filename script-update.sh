d25f33309bd49c03e7bdc8bd32c0fb3c1542fb42
jefelon <ronilson_2006_2@hotmail.com>
2020-10-25 01:08:19 -0500

    cómo actualizar

--- script-update.sh
+++ script-update.sh
@@ -7,12 +7,21 @@ git pull origin "$BRANCH"
 
 docker-compose exec -T fpm$SERVICE_NUMBER composer install
 docker-compose exec -T fpm$SERVICE_NUMBER composer require zanysoft/laravel-zip
-docker-compose exec -T fpm$SERVICE_NUMBER composer require mike42/escpos-php
 docker-compose exec -T fpm$SERVICE_NUMBER php artisan migrate
 docker-compose exec -T fpm$SERVICE_NUMBER php artisan tenancy:migrate
 docker-compose exec -T fpm$SERVICE_NUMBER php artisan cache:clear
 docker-compose exec -T fpm$SERVICE_NUMBER php artisan config:cache
 docker-compose exec -T fpm$SERVICE_NUMBER chmod -R 777 vendor/mpdf/mpdf
+
+#nota habilitar extension extension=intl en docker está en el php.ini del contenedor php-fpm
+#en el contenedror php-fpm ejecutar
+    apt-get update -y
+    apt-get install -y php-intl
+#luego reiniciar nginx en el contenedr nginx con
+    service nginx restart
+
+# ejecutar
+    composer require mike42/escpos-php
 
 
 
