5903384237e7864321bf208f4d8ca7d098ce29c4
jefelon <ronilson_2006_2@hotmail.com>
2020-10-28 01:41:25 -0500

    fix cómo actualizar?

--- script-update.sh
+++ script-update.sh
@@ -16,11 +16,13 @@ docker-compose exec -T fpm$SERVICE_NUMBER chmod -R 777 vendor/mpdf/mpdf
 #nota habilitar extension extension=intl en docker está en el php.ini del contenedor php-fpm
 #en el contenedror php-fpm ejecutar
     apt-get update -y
-    apt-get install -y php-intl
+    apt-get install -y php7.2-intl
+
+    nota: mantener los archivos de configuración, seleccionar 2 y luego N
 #luego reiniciar nginx en el contenedr nginx con
     service nginx restart
 
-# ejecutar
+# ejecutar en contenedor php-fpm
     composer require mike42/escpos-php
 
 
