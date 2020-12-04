6c7990043f82118d64a261b4d2152646c5b415ff
jefelon <ronilson_2006_2@hotmail.com>
2020-12-03 23:59:55 -0500

    add favicon app layout

--- resources/views/tenant/layouts/app.blade.php
+++ resources/views/tenant/layouts/app.blade.php
@@ -10,12 +10,13 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="{{ asset('porto-light/favicon/animated_favicon.gif') }}" type="image/gif" >
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     {{--    <title>{{ config('app.name', 'Facturación Electrónica') }}</title>--}}
-    <title>Facturación Electrónica</title>
+    <title>Facturación Electrónica 2025</title>
 
     <!-- Scripts -->
 
