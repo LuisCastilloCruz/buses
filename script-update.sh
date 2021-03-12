cd81388df19988effa467783289e601150c46552
jefelon <ronilson_2006_2@hotmail.com>
2021-03-12 11:05:36 -0500

    necesario para actualizar sin dolor para los clientes

--- script-update.sh
+++ script-update.sh
@@ -11,6 +11,7 @@ docker-compose exec -T fpm$SERVICE_NUMBER php artisan migrate
 docker-compose exec -T fpm$SERVICE_NUMBER php artisan tenancy:migrate
 docker-compose exec -T fpm$SERVICE_NUMBER php artisan cache:clear
 docker-compose exec -T fpm$SERVICE_NUMBER php artisan config:cache
+docker-compose exec -T fpm$SERVICE_NUMBER php artisan view:clear
 docker-compose exec -T fpm$SERVICE_NUMBER chmod -R 777 vendor/mpdf/mpdf
 
 #nota habilitar extension extension=intl en docker está en el php.ini del contenedor php-fpm
