1e548aac1cf7cbe546ad18e9fade7bb94b1012ca
jefelon <ronilson_2006_2@hotmail.com>
2021-04-07 15:33:27 -0500

    remove dd()

--- app/Imports/InventoryImport.php
+++ app/Imports/InventoryImport.php
@@ -72,7 +72,6 @@ class InventoryImport implements ToCollection
             { //actualiza stock
 
                 if($this->checkInventory($item->id, $warehouse_id)) {
-                    dd("actualizare");
                     $this->updateStockInitial($item->id, $quantity, $warehouse_id);
                 }
                 else{
