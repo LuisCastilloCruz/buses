3959d2398962ab0dbdb98eb9b1aecfc4cfed3ed0
jefelon <ronilson_2006_2@hotmail.com>
2021-04-08 12:34:24 -0500

    Revert "test open link"
    This reverts commit 71fe283179de705b6837af04dceaa71af00f44de.

--- app/Http/Controllers/Tenant/Api/ServiceController.php
+++ app/Http/Controllers/Tenant/Api/ServiceController.php
@@ -76,19 +76,19 @@ class ServiceController extends Controller
                         'message' => $request->number.'-'.$request->series.'| Código: '.$response_code.'|Mensaje: '.$response_description,
                     ]
                 ];
-
+ 
             } else {
                 return [
                     'success' => false,
                     'message' => $response
                 ];
-
-            }
+                
+            }     
     }
     public function validateCpeSunat(Request $request){
-
+        
             $validate_cpe = new ValidateCpeSunat();
-
+                        
             // $acceso = json_decode($datos,true);
             // dd($acceso['access_token']);
 
@@ -110,14 +110,14 @@ class ServiceController extends Controller
                         'message' => $request->series.'-'.$request->number.'| Código: '.$response_code.'|Mensaje: '.$response_description,
                     ]
                 ];
-
+ 
             } else {
                 return [
                     'success' => false,
                     'message' => $response
                 ];
-
-            }
+                
+            }     
     }
 
 
@@ -235,12 +235,5 @@ class ServiceController extends Controller
             ];
         }
     }
-    public function openWhatsapp(Request $request){
-        $num = $request->num;
-        $text = $request->text;
-        header("Location: https://wa.me/$num?text=$text");
-
-    }
-
 
 }
