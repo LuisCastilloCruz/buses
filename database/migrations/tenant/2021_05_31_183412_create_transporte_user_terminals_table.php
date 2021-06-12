660dbda1e62414b90175392fd19c4f753ad8a7f9
jefelon <ronilson_2006_2@hotmail.com>
2021-06-11 19:41:55 -0500

    fix migration trans - user

--- database/migrations/tenant/2021_05_31_183412_create_transporte_user_terminals_table.php
+++ database/migrations/tenant/2021_05_31_183412_create_transporte_user_terminals_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUserTerminalsTable extends Migration
+class TransporteUserTerminalsTable extends Migration
 {
     /**
      * Run the migrations.
