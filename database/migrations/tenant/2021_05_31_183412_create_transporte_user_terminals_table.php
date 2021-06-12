9e19b4973625f2d8fc1fddfa0704eba80b9c56e5
jefelon <ronilson_2006_2@hotmail.com>
2021-06-11 19:46:13 -0500

    refix

--- database/migrations/tenant/2021_05_31_183412_create_transporte_user_terminals_table.php
+++ database/migrations/tenant/2021_05_31_183412_create_transporte_user_terminals_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class TransporteUserTerminalsTable extends Migration
+class CreateTransporteUserTerminalsTable extends Migration
 {
     /**
      * Run the migrations.
