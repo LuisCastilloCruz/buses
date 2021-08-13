accc131c76749851387e0cd47c5248453a9d0957
jefelon <ronilson_2006_2@hotmail.com>
2021-08-13 10:33:24 -0500

    agregar version a css

--- resources/views/tenant/layouts/app.blade.php
+++ resources/views/tenant/layouts/app.blade.php
@@ -41,7 +41,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&display=swap" rel="stylesheet">
 
-    <link rel="stylesheet" href="{{ asset('porto-light/vendor/bootstrap/css/bootstrap.css') }}" />
+    <link rel="stylesheet" href="{{ asset('porto-light/vendor/bootstrap/css/bootstrap.css?id=4') }}" />
     <link rel="stylesheet" href="{{ asset('porto-light/vendor/animate/animate.css') }}" />
     <link rel="stylesheet" href="{{ asset('porto-light/vendor/font-awesome/5.11/css/all.min.css') }}" />
     <link rel="stylesheet" href="{{ asset('porto-light/vendor/select2/css/select2.css') }}" />
@@ -64,12 +64,12 @@
 
     <link rel="stylesheet" type="text/css" href="{{ asset('porto-light/master/style-switcher/style-switcher.css')}}">
 
-    <link rel="stylesheet" href="{{ asset('porto-light/css/theme.css') }}" />
-    <link rel="stylesheet" href="{{ asset('porto-light/css/custom.css') }}" />
-    <link rel="stylesheet" href="{{ asset('porto-light/css/aqpfact.css') }}" />
+    <link rel="stylesheet" href="{{ asset('porto-light/css/theme.css?id=4') }}" />
+    <link rel="stylesheet" href="{{ asset('porto-light/css/custom.css?id=4') }}" />
+    <link rel="stylesheet" href="{{ asset('porto-light/css/aqpfact.css?id=4') }}" />
 
     @if (file_exists(public_path('theme/custom_styles.css')))
-        <link rel="stylesheet" href="{{ asset('theme/custom_styles.css') }}" />
+        <link rel="stylesheet" href="{{ asset('theme/custom_styles.css?id=4') }}" />
     @endif
 
     {{-- @if (file_exists(public_path('theme/custom_styles_ecommerce.css')))
