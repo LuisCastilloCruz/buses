9d38657eca89b0db9e604c6f9befcc8052c1dcbe
jefelon <ronilson_2006_2@hotmail.com>
2021-09-20 12:16:18 -0500

    desactivar migracion llave foranea destino_id

--- database/migrations/tenant/2021_09_18_120345_tenant_drop_transporte_pasajes_destino_id_foreign_to_transporte_pasajes.php
+++ database/migrations/tenant/2021_09_18_120345_tenant_drop_transporte_pasajes_destino_id_foreign_to_transporte_pasajes.php
@@ -14,8 +14,9 @@ class TenantDropTransportePasajesDestinoIdForeignToTransportePasajes extends Mig
     public function up()
     {
         Schema::table('transporte_pasajes', function (Blueprint $table) {
-            $table->dropForeign('transporte_pasajes_destino_id_foreign');
-            $table->foreign('destino_id')->references('id')->on('transporte_destinos');
+            // para corregir decomentar esto y ejecutar, para nuevas empresas ya se corrige
+//            $table->dropForeign('transporte_pasajes_destino_id_foreign');
+//            $table->foreign('destino_id')->references('id')->on('transporte_destinos');
         });
     }
 
@@ -27,7 +28,7 @@ class TenantDropTransportePasajesDestinoIdForeignToTransportePasajes extends Mig
     public function down()
     {
         Schema::table('transporte_pasajes', function (Blueprint $table) {
-            $table->foreign('destino_id')->references('id')->on('transporte_terminales');
+//            $table->foreign('destino_id')->references('id')->on('transporte_terminales');
         });
     }
 }
