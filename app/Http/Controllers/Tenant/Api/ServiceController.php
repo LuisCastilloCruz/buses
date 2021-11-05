f8cb9c37284e2a847b9adec44dedaeca53bbb6aa
jefelon <ronilson_2006_2@hotmail.com>
2021-11-05 13:06:12 -0500

    prueba app

--- app/Http/Controllers/Tenant/Api/ServiceController.php
+++ app/Http/Controllers/Tenant/Api/ServiceController.php
@@ -201,6 +201,8 @@
                 $total
             );
 
+            dd('ruc:'.$company_number.' cod_env:'.$document_type_id.' serie:'.$series. ' number:'.$number .' fecha:'.$date_of_issue. ' total:' .$total);
+
             $response = $validate_cpe->search();
 
             if ($response['success']) {
