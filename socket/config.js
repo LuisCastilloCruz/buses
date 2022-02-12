f55c64a93db254c574bd09bc9858e29e262fc437
jefelon <ronilson_2006_2@hotmail.com>
2022-02-12 18:42:08 -0500

    config general socket-- no se usa

--- socket/config.js
+++ socket/config.js
@@ -1,6 +1,6 @@
 module.exports = config = {
     production:false,
     port: 3000,
-    key: '/etc/letsencrypt/live/pse.aqpfact.pe/privkey.pem',
-    cert: '/etc/letsencrypt/live/pse.aqpfact.pe/fullchain.pem'
+    key: "/etc/letsencrypt/live/fact.domain.com/privkey.pem",
+    cert: "/etc/letsencrypt/live/fact.domain.com/fullchain.pem"
 }
