d2b14a457b1f82c994f084692419c6fc00a14c8a
jefelon <ronilson_2006_2@hotmail.com>
2022-03-15 11:41:54 -0500

    fix

--- modules/Account/Http/Controllers/AccountController.php
+++ modules/Account/Http/Controllers/AccountController.php
@@ -207,7 +207,7 @@ class AccountController extends Controller
         return Document::query()
                                 ->whereBetween('date_of_issue', [$d_start, $d_end])
                                 ->whereIn('currency_type_id', ['PEN','USD'])
-                                ->orderBy('series','asc')
+                                ->orderBy('series','DESC')
                                 ->orderBy('number')
                                 ->get();
 
