97ec1a44d98d80b2b50a4d3d6afd05ace981d1a6
jefelon <ronilson_2006_2@hotmail.com>
2022-05-23 23:23:53 -0500

    fix export siscont anulado

--- modules/Account/Resources/views/accounting/templates/excel_siscont.blade.php
+++ modules/Account/Resources/views/accounting/templates/excel_siscont.blade.php
@@ -60,34 +60,61 @@
     @foreach($records as $row)
         @if($row['state_type_id'] == '11')
             <tr>
-                <td>{{ $row['col_A'] }}</td>
-                <td>{{ $row['date_of_issue'] }}</td>
-                <td>{{ $row['date_of_due'] }}</td>
-                <td>{{ $row['document_type_id'] }}</td>
-                <td>{{ $row['series'] }}</td>
-                <td>{{ $row['number'] }}</td>
-                <td>{{ $row['col_G'] }}</td>
-                <td>0</td>
-                <td>0</td>
-                <td>ANULADA</td>
-                <td>0</td>
-                <td>0</td>
-                <td>0</td>
-                <td>0</td>
-                <td>0</td>
-                <td>0</td>
-                <td>0</td>
-                <td>0</td>
-                <td>{{ $row['col_S'] }}</td>
-                <td>{{ $row['col_T'] }}</td>
-                <td>{{ $row['col_U'] }}</td>
-                <td>{{ $row['col_V'] }}</td>
-                <td>{{ $row['col_W'] }}</td>
-                <td>{{ $row['col_X'] }}</td>
-                <td>{{ $row['col_Y'] }}</td>
-                <td>{{ $row['col_Z'] }}</td>
-                <td>ANULADA</td>
-
+                <!--1--><td>02</td>
+                <!--2--><td>{{ $loop->index+1 }}</td>
+                <!--3--><td>{{ $row['date_of_issue'] }}</td>
+                <!--4--><td>{{ $row['document_type_id'] }}</td>
+                <!--5--><td>{{ $row['series'] }}-{{ $row['number'] }}</td>
+                <!--6--><td>{{ $row['date_of_issue'] }}</td>
+                <!--7--><td>{{ $row['date_of_due'] }}</td>
+                <!--8--><td>00000001</td>
+                <!--9--><td>{{ $row['total_exportation'] }}</td>
+                <!--10--><td>{{ $row['total_taxed'] }}</td>
+                <!--11--><td>{{ $row['total_unaffected'] }}</td>
+                <!--12--><td>0</td>
+                <!--13--><td>{{ $row['total_isc'] }}</td>
+                <!--14--><td>{{ $row['total_igv'] }}</td>
+                <!--15--><td></td>
+                <!--16--><td>{{ $row['total_plastic_bag_taxes'] }}</td>
+                <!--17--><td>{{ $row['currency'] }}</td>
+                <!--18--><td>{{ $row['exchange_rate_sale'] }}</td>
+                <!--19--><td>VENTA</td>
+                <!--20--><td>{{ $row['cta_ingreso'] }}</td>
+                <!--21--><td>{{ $row['cta_igv'] }}</td>
+                <!--22--><td></td>
+                <!--23--><td>{{ $row['cta_x_cobrar'] }}</td>
+                <!--24--><td></td>
+                <!--25--><td></td>
+                <!--26--><td></td>
+                <!--27--><td></td>
+                <!--28--><td></td>
+                <!--29--><td>00000001</td>
+                <!--30--><td>ANULADO</td>
+                <!--31--><td>2</td>
+                <!--32--><td>0</td>
+                <!--33--><td></td>
+                <!--34--><td></td>
+                <!--35--><td></td>
+                <!--36--><td></td>
+                <!--37--><td></td>
+                <!--38--><td></td>
+                <!--39--><td></td>
+                <!--40--><td></td>
+                <!--41--><td></td>
+                <!--42--><td></td>
+                <!--43--><td></td>
+                <!--44--><td></td>
+                <!--45--><td></td>
+                <!--46--><td></td>
+                <!--47--><td></td>
+                <!--48--><td>0</td>
+                <!--49--><td></td>
+                <!--50--><td></td>
+                <!--51--><td></td>
+                <!--52--><td></td>
+                <!--53--><td></td>
+                <!--54--><td></td>
+                <!--55--><td></td>
             </tr>
         @else
             <tr>
