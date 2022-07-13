2e53837d761e9c75de933e1ddaf6ba3c7f52562d
jefelon <ronilson_2006_2@hotmail.com>
2022-07-13 18:01:54 -0500

    fix css movil

--- resources/views/tenant/layouts/partials/header.blade.php
+++ resources/views/tenant/layouts/partials/header.blade.php
@@ -12,12 +12,12 @@
 
     </div>
     <div class="header-right">
-        <a href="https://youtube.com/playlist?list=PLRfWQIWTl4WQnUwpMDBs8iAlcti3s_UNY" target="_blank">
+        <a href="https://youtube.com/playlist?list=PLRfWQIWTl4WQnUwpMDBs8iAlcti3s_UNY" target="_blank" class="youtube">
             <div class="switch switch-sm switch-primary" data-toggle="tooltip" data-placement="bottom" title="Video Tutoriales en Youtube">
                 <img src="{{asset("/logo/youtube.png")}}" alt="">
             </div>
         </a>
-        <span class="separator"></span>
+        <span class="separator youtube"></span>
 
         <ul class="notifications mx-2">
             @if($vc_company->soap_type_id == "01")
@@ -55,14 +55,6 @@
             </li>
         </ul>
         @endif
-        <span class="separator"></span>
-        <ul class="notifications">
-            <li>
-                <a href="{{ route('settings.change_mode') }}" class="notification-icon text-secondary" data-toggle="tooltip" data-placement="bottom" title="Modo noche">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-moon"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>
-                </a>
-            </li>
-        </ul>
         <span class="separator"></span>
         <ul class="notifications">
             <li>
