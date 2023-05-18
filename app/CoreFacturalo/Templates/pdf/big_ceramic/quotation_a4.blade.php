30d64cb7fde856de45cce2b564ba1697f5edd1fc
jefelon <ronilson_2006_2@hotmail.com>
2023-05-18 12:37:33 -0500

    big ceramic - cotizacion

--- app/CoreFacturalo/Templates/pdf/big_ceramic/quotation_a4.blade.php
+++ app/CoreFacturalo/Templates/pdf/big_ceramic/quotation_a4.blade.php
@@ -17,7 +17,6 @@
         $logo = "{$establishment->logo}";
     }
     $config = \App\Models\Tenant\Configuration::first();
-    $miimage = public_path("storage/uploads/fondos/{$fondo}");
 
 @endphp
 <html>
@@ -26,15 +25,22 @@
     {{--<link href="{{ $path_style }}" rel="stylesheet" />--}}
 </head>
 <body>
-<div class="" style="position: absolute; text-align: center; z-index: 0; top: -5px; left: -10px;">
-   @if($fondo!='')
-    <img src="data:{{mime_content_type($miimage)}};base64, {{base64_encode(file_get_contents($miimage))}}" alt="fondo" class="">
-    @endif
-</div>
-<div style="position: absolute; left: 157px;top:7px;width: 79%;text-align:right;">COTIZACIÓN: {{ $tittle }}</div>
-<div style="z-index: 1; position: absolute; left: 157px;padding-left:3px;padding-right: 4px;margin-top: -30px">
+<div style="position: absolute; left: 157px;top:15px;width: 79%;text-align:right;"> <b>COTIZACIÓN: {{ $tittle }}</b></div>
+<div style="z-index: 1; position: absolute; left: 45px;padding-left:3px;padding-right: 20px;margin-top: -30px">
 <table class="full-width">
     <tr>
+        @if($company->logo)
+            <td width="30%" style="text-align:center;">
+                <div class="company_logo_box">
+                    <img src="data:{{mime_content_type(public_path("{$logo}"))}};base64, {{base64_encode(file_get_contents(public_path("{$logo}")))}}" alt="{{$company->name}}" class="company_logo" style="max-width: 250px;">
+                </div>
+            </td>
+        @else
+            <td width="30%">
+                {{--<img src="{{ asset('logo/logo.jpg') }}" class="company_logo" style="max-width: 150px">--}}
+            </td>
+        @endif
+
         <td width="70%">
             <div class="text-center">
                 <h4 class=""><b>{{ $company->name }}</b></h4>
@@ -62,17 +68,6 @@
                 @endisset
             </div>
         </td>
-        @if($company->logo)
-            <td width="30%" style="text-align:center;">
-                <div class="company_logo_box">
-                    <img src="data:{{mime_content_type(public_path("{$logo}"))}};base64, {{base64_encode(file_get_contents(public_path("{$logo}")))}}" alt="{{$company->name}}" class="company_logo" style="max-width: 250px;">
-                </div>
-            </td>
-        @else
-            <td width="30%">
-                {{--<img src="{{ asset('logo/logo.jpg') }}" class="company_logo" style="max-width: 150px">--}}
-            </td>
-        @endif
     </tr>
 </table>
 <table class="full-width mt-4">
@@ -199,11 +194,10 @@
 <table class="full-width mt-1 mb-1">
     <thead class="encabezado">
     <tr class="bg-grey- fondo-ficho">
+        <th class="text-center text-white py-2" width="8%" style="background: {{$color1}}">COD.</th>
         <th class="text-center text-white py-2" width="8%" style="background: {{$color1}}">CANT.</th>
         <th class="text-center text-white py-2" width="10%" style="background: {{$color1}}">UNIDAD</th>
         <th class="text-left text-white py-2" style="background: {{$color1}}">DESCRIPCIÓN</th>
-        <th class="text-left text-white py-2" width="8%" style="background: {{$color1}}">MARCA</th>
-        <th class="text-left text-white py-2" width="9%" style="background: {{$color2}}">MODELO</th>
         <th class="text-right text-white py-2" width="12%" style="background: {{$color2}}">P.UNIT</th>
         <th class="text-right text-white py-2" width="8%" style="background: {{$color2}}">DTO.</th>
         <th class="text-right text-white py-2" width="10%" style="background: {{$color2}}">TOTAL</th>
@@ -212,6 +206,7 @@
     <tbody>
     @foreach($document->items as $row)
         <tr>
+            <td class="text-center align-top">{{ $row->relation_item->internal_id }}</td>
             <td class="text-center align-top">
                 @if(((int)$row->quantity != $row->quantity))
                     {{ $row->quantity }}
@@ -252,8 +247,6 @@
                 @endif
 
             </td>
-            <td class="text-left">{{ $row->item->brand }}</td>
-            <td class="text-left">{{ $row->item->model ?? '' }}</td>
             <td class="text-right align-top">{{ number_format($row->unit_price, 2) }}</td>
             <td class="text-right align-top">
                 @if($row->discounts)
@@ -271,51 +264,51 @@
             <td class="text-right align-top">{{ number_format($row->total, 2) }}</td>
         </tr>
         <tr>
-            <td colspan="8" class="border-bottom"></td>
+            <td colspan="7" class="border-bottom"></td>
         </tr>
     @endforeach
         @if($document->total_exportation > 0)
             <tr>
-                <td colspan="7" class="text-right font-bold">OP. EXPORTACIÓN: {{ $document->currency_type->symbol }}</td>
+                <td colspan="6" class="text-right font-bold">OP. EXPORTACIÓN: {{ $document->currency_type->symbol }}</td>
                 <td class="text-right font-bold">{{ number_format($document->total_exportation, 2) }}</td>
             </tr>
         @endif
         @if($document->total_free > 0)
             <tr>
-                <td colspan="7" class="text-right font-bold">OP. GRATUITAS: {{ $document->currency_type->symbol }}</td>
+                <td colspan="6" class="text-right font-bold">OP. GRATUITAS: {{ $document->currency_type->symbol }}</td>
                 <td class="text-right font-bold">{{ number_format($document->total_free, 2) }}</td>
             </tr>
         @endif
         @if($document->total_unaffected > 0)
             <tr>
-                <td colspan="7" class="text-right font-bold">OP. INAFECTAS: {{ $document->currency_type->symbol }}</td>
+                <td colspan="6" class="text-right font-bold">OP. INAFECTAS: {{ $document->currency_type->symbol }}</td>
                 <td class="text-right font-bold">{{ number_format($document->total_unaffected, 2) }}</td>
             </tr>
         @endif
         @if($document->total_exonerated > 0)
             <tr>
-                <td colspan="7" class="text-right font-bold">OP. EXONERADAS: {{ $document->currency_type->symbol }}</td>
+                <td colspan="6" class="text-right font-bold">OP. EXONERADAS: {{ $document->currency_type->symbol }}</td>
                 <td class="text-right font-bold">{{ number_format($document->total_exonerated, 2) }}</td>
             </tr>
         @endif
         @if($document->total_taxed > 0)
             <tr>
-                <td colspan="7" class="text-right font-bold">OP. GRAVADAS: {{ $document->currency_type->symbol }}</td>
+                <td colspan="6" class="text-right font-bold">OP. GRAVADAS: {{ $document->currency_type->symbol }}</td>
                 <td class="text-right font-bold">{{ number_format($document->total_taxed, 2) }}</td>
             </tr>
         @endif
        @if($document->total_discount > 0)
             <tr>
-                <td colspan="7" class="text-right font-bold">{{(($document->total_prepayment > 0) ? 'ANTICIPO':'DESCUENTO TOTAL')}}: {{ $document->currency_type->symbol }}</td>
+                <td colspan="6" class="text-right font-bold">{{(($document->total_prepayment > 0) ? 'ANTICIPO':'DESCUENTO TOTAL')}}: {{ $document->currency_type->symbol }}</td>
                 <td class="text-right font-bold">{{ number_format($document->total_discount, 2) }}</td>
             </tr>
         @endif
         <tr>
-            <td colspan="7" class="text-right font-bold">IGV: {{ $document->currency_type->symbol }}</td>
+            <td colspan="6" class="text-right font-bold">IGV: {{ $document->currency_type->symbol }}</td>
             <td class="text-right font-bold">{{ number_format($document->total_igv, 2) }}</td>
         </tr>
         <tr>
-            <td colspan="7" class="text-right font-bold">TOTAL A PAGAR: {{ $document->currency_type->symbol }}</td>
+            <td colspan="6" class="text-right font-bold">TOTAL A PAGAR: {{ $document->currency_type->symbol }}</td>
             <td class="text-right font-bold">{{ number_format($document->total, 2) }}</td>
         </tr>
     </tbody>
